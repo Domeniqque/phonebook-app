f73d91d2c886b838091990eed988a63b7c9e3d3a
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-07-05 17:37:21 -0400

    add phone placehlder

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useMemo, useEffect } from 'react';
 
 import Select from '../../components/Select';
 import { useLocale } from '../../hooks/locale';
--- src/utils/parsePhoneNumber.ts
+++ src/utils/parsePhoneNumber.ts
@@ -12,18 +12,27 @@ function trim(number: string): string {
 export function parsePhone(phoneNumber: PhoneNumber | string): PhoneParsed {
   const fullNumber =
     (phoneNumber as PhoneNumber)?.formatNational() ?? phoneNumber;
+  const rawNumber = fullNumber.replace('-', ' ') || '';
 
-  let number = fullNumber || '';
+  let number = rawNumber;
   let areaCode = '';
 
-  // If number is formated with prefix like: (69) 99999-9999
-  if (fullNumber.search(/\)/) !== -1) {
-    const [code, splitedNumber] = fullNumber.split(')');
-    number = splitedNumber;
+  // If number is formated with prefix ()- like: (69) 99999-9999
+  if (rawNumber.search(/\)/) !== -1) {
+    const [code, splitedNumber] = rawNumber.split(')');
+    number = trim(splitedNumber);
     areaCode = trim(code);
+  } else {
+    const [code, ...rest] = rawNumber.split(' ');
+
+    // If number is formated with prefix like: 69 99999 9999
+    if (rest.length > 0) {
+      areaCode = trim(code);
+      number = trim(rest.join(' '));
+    }
   }
 
-  number = trim(number);
+  console.log(fullNumber, { areaCode, number });
 
   return {
     number: Number(number),
