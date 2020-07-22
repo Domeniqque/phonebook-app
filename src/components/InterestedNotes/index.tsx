9e83f8d7bc645a97bde90d90f0a2cf9d54a586cb
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-07-22 10:46:31 -0400

    wip remove notes

--- src/components/InterestedNotes/index.tsx
+++ src/components/InterestedNotes/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import crashlytics from '@react-native-firebase/crashlytics';
+import Icon from 'react-native-vector-icons/Feather';
 
 import FullModal from '../FullModal';
 
@@ -14,12 +15,42 @@ import { useAlert } from '../../hooks/alert';
 import getValidationErrors from '../../utils/getValidationErrors';
 import getRealm from '../../services/realm';
 import { InterestedProps } from '../../schemas/InterestedSchema';
+import { NoteProps } from '../../schemas/NoteSchema';
+import uuid from '../../utils/uuid';
 
-import { Container, Title, AddButton, AddButtonText, Header } from './styles';
+import {
+  Container,
+  Title,
+  AddButton,
+  AddButtonText,
+  Header,
+  Content,
+  NoteItem,
+  NoteText,
+  NoteDate,
+  DeleteNote,
+} from './styles';
 
 interface InterestedNotesProps {
   interestedId: string | undefined;
   interestedName: string | undefined;
+}
+
+type NoteResult = Realm.Results<NoteProps & Realm.Object>;
+
+async function loadNotes(
+  interestedId: string | undefined,
+): Promise<NoteResult> {
+  if (!interestedId) return {} as NoteResult;
+
+  const realm = await getRealm();
+
+  const data = realm
+    .objects<NoteProps>('Note')
+    .filtered(`interested.id = "${interestedId}"`)
+    .sorted('date', true);
+
+  return data;
 }
 
 const InterestedNotes: React.FC<InterestedNotesProps> = ({
@@ -28,15 +59,23 @@ const InterestedNotes: React.FC<InterestedNotesProps> = ({
 }) => {
   const [addMode, setAddMode] = useState(false);
   const [date, setDate] = useState<Date>(new Date());
+  const [notes, setNotes] = useState<NoteResult>({} as NoteResult);
+
   const formRef = useRef<FormHandles>(null);
 
   const { trans } = useLocale();
   const { alert, success } = useAlert();
 
+  useEffect(() => {
+    loadNotes(interestedId).then(setNotes);
+  }, [interestedId]);
+
   const handleSaveNote = useCallback(
     async ({ note }) => {
       if (!interestedId) return;
       formRef.current?.setErrors({});
+
+      crashlytics().log('Cadastrando uma nota');
 
       try {
         const schema = Yup.object({
@@ -45,42 +84,71 @@ const InterestedNotes: React.FC<InterestedNotesProps> = ({
 
         await schema.validate({ note }, { abortEarly: false });
 
-        const data = {
-          text: note,
-          pinned: false,
-          date,
-        };
-
-        console.log(data);
-
         const realm = await getRealm();
 
-        realm.write(() => {
+        realm.write(async () => {
+          success();
+
           const interested = realm.objectForPrimaryKey<InterestedProps>(
             'Interested',
             interestedId,
           );
 
-          interested?.notes.push(data);
+          interested?.notes.push({
+            id: uuid(),
+            text: note,
+            pinned: false,
+            date,
+          });
 
           setAddMode(false);
-          success();
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError)
           formRef.current?.setErrors(getValidationErrors(err));
         else {
+          setAddMode(false);
+          crashlytics().recordError(err);
+
           alert({
             title: trans('defaultError.title'),
             text: trans('defaultError.text'),
             confirmText: 'OK',
           });
-
-          crashlytics().recordError(err);
         }
       }
     },
-    [alert, trans, date, interestedId],
+    [alert, trans, date, success, interestedId],
+  );
+
+  const handleDeleteNote = useCallback(
+    async (id: string, text: string) => {
+      async function destroy(): Promise<void> {
+        const realm = await getRealm();
+
+        success();
+        realm.write(() => {
+          const note = realm
+            .objects<NoteProps>('Note')
+            .filtered(`id = "${id}"`);
+
+          if (note) {
+            realm.delete(note);
+          }
+        });
+      }
+
+      alert({
+        title: 'Deseja excluir esta nota?',
+        text: text.length < 110 ? text : `${text.substr(0, 110)}...`,
+        confirmText: 'Sim, excluir',
+        cancelText: 'Cancelar',
+        onConfirm: (): void => {
+          destroy();
+        },
+      });
+    },
+    [success, alert],
   );
 
   if (addMode) {
@@ -121,6 +189,18 @@ const InterestedNotes: React.FC<InterestedNotesProps> = ({
           <AddButtonText>{trans('interested.notes.btnAdd')}</AddButtonText>
         </AddButton>
       </Header>
+
+      <Content>
+        {notes &&
+          notes.map(note => (
+            <NoteItem key={note.id}>
+              <NoteText>{note.text}</NoteText>
+              <DeleteNote onPress={() => handleDeleteNote(note.id, note.text)}>
+                <Icon name="trash" size={20} color="#000" />
+              </DeleteNote>
+            </NoteItem>
+          ))}
+      </Content>
     </Container>
   );
 };
